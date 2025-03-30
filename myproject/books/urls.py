4d9068e33c03601164b5a3afffd07c701c7da47a
Mattv78 <160672579+Mattv78@users.noreply.github.com>
2025-03-30 14:51:20 -0400

    cleaning out old files

--- myproject/books/urls.py
+++
@@ -1,8 +0,0 @@
-# books/urls.py
-from django.urls import path
-from .views import books_by_genre,all_books
-
-urlpatterns = [
-    path('books/', books_by_genre, name='books_by_genre'),
-    path('all-books/', all_books, name='all_books'),
-]
--- reviews/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import add_review, get_reviews
-
-urlpatterns = [
-    path("add/", add_review, name="add_review"),  # URL for adding a review
-    path("<str:book_id>/", get_reviews, name="get_reviews"),  # URL for fetching reviews
-]
